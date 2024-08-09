579bde67eea69bfb3ecf0dbb5e88bdcbe7282edd
bada <silverzoo030@gmail.com>
2024-08-09 18:11:54 +0900

    :sparkles: thymeleaf blog project basic

--- src/main/java/com/elice/practice/repository/BlogRepository.java
+++
@@ -1,8 +0,0 @@
-package com.elice.practice.repository;
-
-import com.elice.practice.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogRepository extends JpaRepository<Article, Long> {
-}
-
